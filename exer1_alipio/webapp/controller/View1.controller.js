eac7877c0138938a4d8ee0118fb66fb0b88b91c8
Christian Jay Alipio <christian.jay.alipio@accenture.com>
2025-08-08 04:05:49 +0000

    day 1 commit

--- exer1_alipio/webapp/controller/View1.controller.js
+++ exer1_alipio/webapp/controller/View1.controller.js
@@ -5,6 +5,19 @@ sap.ui.define([
 
     return Controller.extend("com.ui5.trng.exer1alipio.controller.View1", {
         onInit() {
+        },
+
+        onClearPress: function () {
+            // Clear Input fields
+            this.byId("input0").setValue("");
+            this.byId("input1").setValue("");
+            this.byId("input2").setValue("");
+            this.byId("input5").setValue("");
+            this.byId("input6").setValue("");
+        
+            // Clear ComboBox selection
+            this.byId("box0").setSelectedKey("");
         }
+        
     });
 });
