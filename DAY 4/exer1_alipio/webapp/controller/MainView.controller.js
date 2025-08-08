36d97a017acde75ceaf157a05dd0401280c58a9c
Christian Jay Alipio <christian.jay.alipio@accenture.com>
2025-08-08 07:50:37 +0000

    Modified day 4 exercise

--- DAY 4/exer1_alipio/webapp/controller/MainView.controller.js
+++ DAY 4/exer1_alipio/webapp/controller/MainView.controller.js
@@ -1,24 +1,62 @@
 sap.ui.define([
-
     "sap/ui/core/mvc/Controller",
-    
     "sap/m/MessageToast"
-    
-    ],
-    
-    @param {typeof sap.ui.core.mvc.Controller} Controller
+], function (Controller, MessageToast) {
+    "use strict";
 
-    function (Controller, MessageToast){
-        onAddItem: function (){
+    return Controller.extend("your.namespace.controller.ViewName", {
+        
+        onInit: function () {
+        },
 
+        onAddItem: function () {
             this.fnDisplayMsg("Add button pressed");
-            
-            },
-            
-            
-            fnDisplayMsg: function (sMsg){
-            
+        },
+
+        fnDisplayMsg: function (sMsg) {
             MessageToast.show(sMsg);
+        },
+
+        onChangeMOP: function (oEvent) {
+            var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
+
+            var oMobileLabel = this.getView().byId("idLblPhone");
+            var oMobileInput = this.getView().byId("idInputPhone");
+
+            if (sSelectedKey === "GCASH") {
+                oMobileLabel.setVisible(true);
+                oMobileInput.setVisible(true);
+            } else {
+                oMobileLabel.setVisible(false);
+                oMobileInput.setVisible(false);
+            }
+        },
+
+        onPressCheckout: function () {
+            var oInputFNameValue = this.getView().byId("idInptFName").getValue();
+
+            if (oInputFNameValue === "") {
+                MessageToast.show("Required Field is blank");
+            }
             
-            },
-    }
+        },
+        onAddItem: function(){
+            var oTextBundle =
+            this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            var sMsg = oTextBundle.getText("addButtonMsg");
+            this.fnDisplayMsg(sMsg);
+        },
+
+        onClearPress: function () {
+            this.byId("input0").setValue("");
+            this.byId("input1").setValue("");
+            this.byId("input2").setValue("");
+            this.byId("input5").setValue("");
+            this.byId("input6").setValue("");
+
+            this.byId("box0").setSelectedKey("");
+
+            MessageToast.show("Form cleared.");
+        }
+    });
+});
