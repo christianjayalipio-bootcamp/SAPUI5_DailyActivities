82330bc2a1058fc583f85aa21a2728855b5eafa0
christianjayalipio-bootcamp <christian.jay.alipio@accenture.com>
2025-08-13 16:15:00 +0800

    I COMMIT THIS DAY

--- DAY 5/jsonbinding/webapp/controller/JSONBinding.controller.js
+++ DAY 5/jsonbinding/webapp/controller/JSONBinding.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sapips/training/jsonbinding/model/formatter"
-], function(Controller, JSONModel, formatter) {
+], function(Controller, JSONModel, formatter, mobileLibrary) {
     "use strict";
 
     return Controller.extend("sapips.training.jsonbinding.controller.JSONBinding", {
@@ -10,9 +10,9 @@ sap.ui.define([
         formatter: formatter,
 
         onInit: function() {
-            // Create and set the default model for employee data
+
             const oEmployeeData = new JSONModel({
-                EID: "christian.jay.alipio",
+                EID: " ",
                 Enabled: true,
                 Address: {
                     Street: "Nangalisan",
@@ -25,7 +25,7 @@ sap.ui.define([
             });
             this.getView().setModel(oEmployeeData);
 
-            // Create and set the named model for product data
+
             const oProductModel = new JSONModel("/model/Products.json");
             this.getView().setModel(oProductModel, "ProductsModel");
         },
