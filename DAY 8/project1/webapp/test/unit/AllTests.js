c144f84d1c4127f38429113ec3c1fb8d5ccff2b7
christianjayalipio-bootcamp <christian.jay.alipio@accenture.com>
2025-08-15 09:28:52 +0800

    this is for day 8

---
+++ DAY 8/project1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/ui5/trng/project1/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
---
+++ DAY 8/project1/webapp/test/unit/controller/MainView.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"com/ui5/trng/project1/controller/MainView.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("MainView Controller");
+
+	QUnit.test("I should test the MainView controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
